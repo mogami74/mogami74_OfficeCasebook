24630d2166ec8dfb6a51a30174fbfa03ff9fa79b
新保輝之 <mogami74@gmail.com>
2017-12-07 10:09:38 +0900

    fotterの処理をモジュール化。

--- index.php
+++ index.php
@@ -9,14 +9,5 @@
 				<?php get_template_part('sidebar'); ?>
 			</aside>
 		</main>
-		<footer id="l-blog-container__footer" class="l-contents">
-		<?php wp_footer(); ?>
-			<small>&copy;mogami74;</small>
-		</footer>
-	</div> <!--full-wrap-->
-	<div class="l-ad-full">
-		<p class="c-ad-wrap__sponsored">sponsored link</p>
-			<p style="border:1px solid blue;width:320px;height: 100px;">Google AdSense</p>
-		</div>
-</body>
-</html>
+
+<?php get_template_part('footer'); ?>
