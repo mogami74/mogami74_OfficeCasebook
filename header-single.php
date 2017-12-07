91d07882ee5bb4e7ccc7ceaf37316c2634af60f0
新保輝之 <mogami74@gmail.com>
2017-12-07 11:19:17 +0900

    上下のAdsenseユニットが表示されない不具合を解消。

--- header-single.php
+++ header-single.php
@@ -29,7 +29,7 @@
          data-ad-slot="7596323228"
          data-ad-format="auto"></ins>
     <script>
-    (adsbygoogle = window.adsbygoogle || []).push({});
+    jQuery(document).ready(function(){(adsbygoogle = window.adsbygoogle || []).push({});});
     </script>
   </div>
 	<div id="l-blog-container">
